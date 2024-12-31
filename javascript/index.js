27d77ba6adf5cbb7cee823fad3e8a7f141a5ea83
kahesr <60002689+kahesr@users.noreply.github.com>
2024-12-31 18:55:29 +0330

    Added the functionality to the select dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -3,9 +3,7 @@ function updateTime() {
 
   cities.forEach((city) => {
     let timezone = city.dataset.timezone; //Australia/Sydney
-
     let localTime = moment().tz(timezone);
-
     let date = localTime.format("dddd Do YYYY");
     let time = localTime.format("h:mm:ss [<small]>A[</small]");
 
@@ -13,6 +11,33 @@ function updateTime() {
     city.querySelector(".time").innerHTML = time;
   });
 }
+
+function updateCityList(event) {
+  let selectedTimezone = event.target.value;
+  let cities = document.querySelectorAll(".city");
+  let cityExists = Array.from(cities).some(
+    (city) => city.dataset.timezone === selectedTimezone
+  ); // Check if the city is already in the list
+
+  if (event.target.value && !cityExists) {
+    let cityName = selectedTimezone.replace("_", " ").split(`/`)[1];
+    let localTime = moment().tz(selectedTimezone);
+    let date = localTime.format("dddd Do YYYY");
+    let time = localTime.format("h:mm:ss [<small]>A[</small]");
+    let cityList = document.querySelector(".cityList");
+
+    cityList.innerHTML += `<div class="city" data-timezone="${selectedTimezone}">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${date}</div>
+    </div>
+    <div class="time">${time}</div>
+    </div>`;
+  }
+}
+
+let selectedCity = document.querySelector("#selected-city");
+selectedCity.addEventListener("change", updateCityList);
 
 updateTime();
 setInterval(updateTime, 1000);
